3a9705da3cece55328494ddb00f76beeacee824f
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-12-03 02:11:39 -0300

    03122022
    Configuração do tema escuro, criação do event card generico e criação da interface de busca.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,128 @@
-import { Container, FormControl, Input } from '@chakra-ui/react';
-import './App.css';
-import { ColorModeSwitcher } from './themeControl/teste';
+import { Container, Flex, Input, IconButton, InputGroup, Divider, InputLeftElement, VStack, Heading } from '@chakra-ui/react';
+import React from 'react';
+import { FaPlus, FaSearch } from "react-icons/fa"
+import CardEvent from './components/event';
+import { ColorModeSwitcher } from './themeControl';
 
 function App() {
-  return (<div className='App'>
-    <span>Worklist</span>
-    <FormControl>
-      <Container maxW='container.sm'>
-        <Input placeholder='Event' />
-        <ColorModeSwitcher/>
-      </Container>
-    </FormControl>
-  </div>);
+  return (<>
+    <Container maxW='container.md' minW='container.md' backdropFilter='blur(10px)'>
+      <Flex justifyContent='center'>
+        <Heading as='h1' size='4xl' margin='15px'>Worklist</Heading>
+      </Flex>
+      <Flex>
+        <InputGroup>
+          <InputLeftElement pointerEvents='none' children={<FaSearch />} />
+          <Input placeholder='Event' />
+        </InputGroup>
+        <IconButton
+          icon={<FaPlus />}
+          variant="ghost"
+          color="current"
+          marginLeft="2"
+          size="md"
+          fontSize="lg"
+        />
+        <ColorModeSwitcher />
+      </Flex>
+      <Divider marginTop='10px' marginBottom='10px' />
+      <VStack spacing={4}>
+        {events.map((e, i) => <CardEvent key={i} {...e} />)}
+      </VStack>
+    </Container>
+  </>);
 }
+
+const events = [{
+  title: 'titulo',
+  DateTimeLocal: 'datetime-local',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta vitae nisi ut semper. Phasellus facilisis augue at ultricies porttitor. Proin ac aliquet lectus, in volutpat orci. Vestibulum at est eu felis tempor dignissim vitae nec ligula. Vivamus molestie ligula felis, ultrices viverra nisi placerat ac. Duis congue ante mi.',
+  tag: [
+    { color: 'gray', name: 'tag' },
+    { color: 'red', name: 'tag' },
+    { color: 'orange', name: 'tag' },
+    { color: 'yellow', name: 'tag' },
+    { color: 'green', name: 'tag' },
+    { color: 'teal', name: 'tag' },
+    { color: 'blue', name: 'tag' },
+    { color: 'cyan', name: 'tag' },
+    { color: 'purple', name: 'tag' },
+    { color: 'pink', name: 'tag' }]
+}, {
+  title: 'titulo',
+  DateTimeLocal: 'datetime-local',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta vitae nisi ut semper. Phasellus facilisis augue at ultricies porttitor. Proin ac aliquet lectus, in volutpat orci. Vestibulum at est eu felis tempor dignissim vitae nec ligula. Vivamus molestie ligula felis, ultrices viverra nisi placerat ac. Duis congue ante mi.',
+  tag: [
+    { color: 'gray', name: 'tag' },
+    { color: 'red', name: 'tag' },
+    { color: 'orange', name: 'tag' },
+    { color: 'yellow', name: 'tag' },
+    { color: 'green', name: 'tag' },
+    { color: 'teal', name: 'tag' },
+    { color: 'blue', name: 'tag' },
+    { color: 'cyan', name: 'tag' },
+    { color: 'purple', name: 'tag' },
+    { color: 'pink', name: 'tag' }]
+}, {
+  title: 'titulo',
+  DateTimeLocal: 'datetime-local',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta vitae nisi ut semper. Phasellus facilisis augue at ultricies porttitor. Proin ac aliquet lectus, in volutpat orci. Vestibulum at est eu felis tempor dignissim vitae nec ligula. Vivamus molestie ligula felis, ultrices viverra nisi placerat ac. Duis congue ante mi.',
+  tag: [
+    { color: 'gray', name: 'tag' },
+    { color: 'red', name: 'tag' },
+    { color: 'orange', name: 'tag' },
+    { color: 'yellow', name: 'tag' },
+    { color: 'green', name: 'tag' },
+    { color: 'teal', name: 'tag' },
+    { color: 'blue', name: 'tag' },
+    { color: 'cyan', name: 'tag' },
+    { color: 'purple', name: 'tag' },
+    { color: 'pink', name: 'tag' }]
+}, {
+  title: 'titulo',
+  DateTimeLocal: 'datetime-local',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta vitae nisi ut semper. Phasellus facilisis augue at ultricies porttitor. Proin ac aliquet lectus, in volutpat orci. Vestibulum at est eu felis tempor dignissim vitae nec ligula. Vivamus molestie ligula felis, ultrices viverra nisi placerat ac. Duis congue ante mi.',
+  tag: [
+    { color: 'gray', name: 'tag' },
+    { color: 'red', name: 'tag' },
+    { color: 'orange', name: 'tag' },
+    { color: 'yellow', name: 'tag' },
+    { color: 'green', name: 'tag' },
+    { color: 'teal', name: 'tag' },
+    { color: 'blue', name: 'tag' },
+    { color: 'cyan', name: 'tag' },
+    { color: 'purple', name: 'tag' },
+    { color: 'pink', name: 'tag' }]
+}, {
+  title: 'titulo',
+  DateTimeLocal: 'datetime-local',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta vitae nisi ut semper. Phasellus facilisis augue at ultricies porttitor. Proin ac aliquet lectus, in volutpat orci. Vestibulum at est eu felis tempor dignissim vitae nec ligula. Vivamus molestie ligula felis, ultrices viverra nisi placerat ac. Duis congue ante mi.',
+  tag: [
+    { color: 'gray', name: 'tag' },
+    { color: 'red', name: 'tag' },
+    { color: 'orange', name: 'tag' },
+    { color: 'yellow', name: 'tag' },
+    { color: 'green', name: 'tag' },
+    { color: 'teal', name: 'tag' },
+    { color: 'blue', name: 'tag' },
+    { color: 'cyan', name: 'tag' },
+    { color: 'purple', name: 'tag' },
+    { color: 'pink', name: 'tag' }]
+}, {
+  title: 'titulo',
+  DateTimeLocal: 'datetime-local',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta vitae nisi ut semper. Phasellus facilisis augue at ultricies porttitor. Proin ac aliquet lectus, in volutpat orci. Vestibulum at est eu felis tempor dignissim vitae nec ligula. Vivamus molestie ligula felis, ultrices viverra nisi placerat ac. Duis congue ante mi.',
+  tag: [
+    { color: 'gray', name: 'tag' },
+    { color: 'red', name: 'tag' },
+    { color: 'orange', name: 'tag' },
+    { color: 'yellow', name: 'tag' },
+    { color: 'green', name: 'tag' },
+    { color: 'teal', name: 'tag' },
+    { color: 'blue', name: 'tag' },
+    { color: 'cyan', name: 'tag' },
+    { color: 'purple', name: 'tag' },
+    { color: 'pink', name: 'tag' }]
+},]
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider resetCSS={true}>
         <App />
     </ChakraProvider>
   </React.StrictMode>
